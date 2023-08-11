72d9643cb50e541d141ef155c60d15f7c2ea1038
Juanequex <ramon.g.uz37@gmail.com>
2023-08-11 11:59:52 -0600

    new background

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -12,9 +12,8 @@ const AboutUs = () => {
           <h2 className="about-us__title">
             who the hell is Juanequex?
           </h2>
-          <p className="about-us__subtext">I have a Software Engineering degree from the Universidad de Colima. My working experience revolves around Ruby on Rails, JS, and, React JS</p>
-          <p className="about-us__subtext">My professional career started at BrightCoders Academy with a successful course on Ruby on Rails. A few months after, I began working at MagmaLabs.</p>
-          <p className="about-us__subtext">My current role is a RoR developer at MagmaLabs, and I also mentor at BrightCoders Academy. I'm part of the Playas in Tech community in Manzanillo and enjoy spending my time creating content and code for my personal brand's growth</p>
+          <p className="about-us__subtext">My professional journey took flight at BrightCoders Academy, where I successfully completed a course on Ruby on Rails. A few months later, I embarked on my tenure at MagmaLabs.</p>
+          <p className="about-us__subtext">Presently, I serve as a Ruby On Rails developer at MagmaLabs, concurrently mentoring at BrightCoders Academy. I'm an active participant in the Playas in Tech community in Manzanillo, dedicating time to crafting content and code to foster the growth of my personal brand.</p>
         </div>
       </div>
     </section>
